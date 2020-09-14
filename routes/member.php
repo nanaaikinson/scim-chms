2919d5e004fac031e279140a33b2b81de0d1b495
Nana Kwesi Ofosu-Aikins <61139144+nanaaikinson@users.noreply.github.com>
2020-09-14 08:05:17 +0000

    Update member.php

--- routes/member.php
+++ routes/member.php
@@ -9,8 +9,11 @@ Route::middleware(["json.response"])->group(function () {
     Route::post("/verify-token", "AuthController@verifyToken");
   });
 
-  Route::middleware("auth:api")->group(function() {
+  /*Route::middleware("auth:api")->group(function() {
     Route::get("/sermons", "SermonController@sermons");
     Route::post("/prayer-request", "PrayerRequestController@store");
-  });
+  });*/
+
+  Route::get("/sermons", "SermonController@sermons");
+  Route::post("/prayer-request", "PrayerRequestController@store");
 });
