c1c889397d88888a7781c8468b813a9408c5661a
Nana Kwesi Ofosu-Aikins <61139144+nanaaikinson@users.noreply.github.com>
2020-09-15 13:03:04 +0000

    Mail config

--- app/Mail/MemberRegisteredMail.php
+++ app/Mail/MemberRegisteredMail.php
@@ -30,7 +30,8 @@ class MemberRegisteredMail extends Mailable implements ShouldQueue
    */
   public function build()
   {
-    return $this->subject("{$this->data['token']} is login/email verification code")
+    return $this->from("noreply@salvationclincchms.com", "SCIM")
+      ->subject("{$this->data['token']} is login/email verification code")
       ->view('email.member.created');
 
   }
