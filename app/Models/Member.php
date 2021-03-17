b6c15196c1701e8abd79c994908443421296ca93
Nana Aikinson <nanaaikinson2020@gmail.com>
2021-03-17 18:36:26 +0000

    Passport removal

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,18 +2,39 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Passport\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class Member extends Authenticatable
+class Member extends Authenticatable implements JWTSubject
 {
-  use HasApiTokens, Notifiable;
+  use Notifiable;
 
   protected $guarded = [];
 
-  public function avatar()
+  public function avatar(): MorphOne
   {
     return $this->morphOne('App\Models\Image', 'imageable');
+  }
+
+  /**
+   * Get the identifier that will be stored in the subject claim of the JWT.
+   *
+   * @return mixed
+   */
+  public function getJWTIdentifier()
+  {
+    return $this->getKey();
+  }
+
+  /**
+   * Return a key value array, containing any custom claims to be added to the JWT.
+   *
+   * @return array
+   */
+  public function getJWTCustomClaims(): array
+  {
+    return [];
   }
 }
