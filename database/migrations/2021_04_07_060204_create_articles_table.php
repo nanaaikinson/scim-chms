d8b47744388be7ae23e8acc6f824e28f3f2ed55c
Nana Aikinson <nanaaikinson2020@gmail.com>
2021-04-07 06:48:32 +0000

    Update 2021_04_07_060204_create_articles_table.php

--- database/migrations/2021_04_07_060204_create_articles_table.php
+++ database/migrations/2021_04_07_060204_create_articles_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateArticlesTable extends Migration
@@ -30,6 +31,8 @@ class CreateArticlesTable extends Migration
    */
   public function down()
   {
+    DB::statement('SET FOREIGN_KEY_CHECKS=0;');
     Schema::dropIfExists('articles');
+    DB::statement('SET FOREIGN_KEY_CHECKS=1;');
   }
 }
