b762ae80a01767a62bce76af9f6e962494cf75f7
Nana Aikinson <nanaaikinson2020@gmail.com>
2021-04-07 06:47:16 +0000

    Refactor comments, comment_replies and likes tables

--- database/migrations/2021_04_07_063305_create_comment_reply_table.php
+++ database/migrations/2021_04_07_063305_create_comment_reply_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateCommentReplyTable extends Migration
@@ -29,6 +30,8 @@ class CreateCommentReplyTable extends Migration
    */
   public function down()
   {
+    DB::statement('SET FOREIGN_KEY_CHECKS=0;');
     Schema::dropIfExists('comment_replies');
+    DB::statement('SET FOREIGN_KEY_CHECKS=1;');
   }
 }
