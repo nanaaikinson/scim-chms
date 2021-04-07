08792dd4ac94abdfeebfa7b2514a28e28ea8fc95
Nana Aikinson <nanaaikinson2020@gmail.com>
2021-04-07 08:01:40 +0000

    Update users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,14 @@ class CreateUsersTable extends Migration
       $table->string('last_name');
       $table->string('email')->unique();
       $table->string('password');
-      $table->unsignedBigInteger('mask')->unique();
+      $table->string("telephone")->nullable();
+      $table->boolean("is_facebook")->default(false);
+      $table->boolean("is_google")->default(false);
+      $table->string("token")->nullable();
+      $table->dateTime("token_at")->nullable();
+      $table->string("country")->nullable();
+      $table->string("role")->nullable();
+      $table->string('mask')->unique();
       $table->rememberToken();
       $table->timestamps();
       $table->softDeletes();
